aadeeee667a0edaec65c85e2cf4ac02e1545ffe9
jerssonfuentes <jersonfuentesparra2017@gmail.com>
2025-03-21 12:42:49 -0500

    feat: :poop: OHH NO

--- script.js
+++ script.js
@@ -1,30 +1,28 @@
+function esEquilibrada(expresion) {
+  const pila = [];
+  const delimitadores = { "(": ")", "{": "}", "[": "]" };
+
+  for (let caracter of expresion) {
+    if (delimitadores[caracter]) {
+    
+      pila.push(caracter);
+    } else if (Object.values(delimitadores).includes(caracter)) {
+    
+      if (pila.length === 0 || delimitadores[pila.pop()] !== caracter) {
+        return false;
+      }
+    }
+  }
+
+  return pila.length === 0;
+}
 function validarDelimitadores() {
     const expresion = document.getElementById("expression").value;
     const resultadoElement = document.getElementById("resultado");
-  
-    function esEquilibrada(expresion) {
-      const pila = [];
-      const delimitadores = { "(": ")", "{": "}", "[": "]" };
-  
-      for (let caracter of expresion) {
-        if (delimitadores[caracter]) {
-        
-          pila.push(caracter);
-        } else if (Object.values(delimitadores).includes(caracter)) {
-        
-          if (pila.length === 0 || delimitadores[pila.pop()] !== caracter) {
-            return false;
-          }
-        }
-      }
-  
-      return pila.length === 0;
-    }
-  
-
+    console.log(esEquilibrada(expresion));
     if (esEquilibrada(expresion)) {
-      resultadoElement.textContent = " La expresión está equilibrada.";
+      resultadoElement.innerText = " La expresión está equilibrada.";
     } else {
-      resultadoElement.textContent = " La expresión NO está equilibrada.";
+      resultadoElement.innerText = " La expresión NO está equilibrada.";
     }
   }
